6ad033122bf62bf45a51c4c67d59e7a1f28ac09d
AndreGleichner <andre.gleichner@gmail.com>
2019-01-17 19:34:36 +0100

    Renamed Err field

--- internal/server/file_download.go
+++ internal/server/file_download.go
@@ -6,6 +6,15 @@ import (
 	"log"
 )
 
+// message NegotiateDownloadFilesReq {
+// }
+// message NegotiateDownloadFilesResp {
+//     message File {
+//         string  filename = 1; // relative to out dir
+//         bytes   sha256 = 2;
+//     }
+//     repeated File   files = 1; // files the server wants the client to download
+// }
 // NegotiateDownloadFiles shall be called after any RunCommand() completed to possibly download (further) files via one or more DownloadFile().
 func (s *TaurosServer) NegotiateDownloadFiles(ctx context.Context, req *api.NegotiateDownloadFilesReq) (resp *api.NegotiateDownloadFilesResp, err error) {
 	log.Printf("NegotiateDownloadFiles")
@@ -13,6 +22,29 @@ func (s *TaurosServer) NegotiateDownloadFiles(ctx context.Context, req *api.Nego
 	return &api.NegotiateDownloadFilesResp{}, nil
 }
 
+// message DownloadFileReq {
+//     message Meta {
+//         string  filename = 1; // relative to out dir
+//     }
+// }
+
+// message DownloadFileRespStream {
+//     message Meta {
+//         int32   filesize = 1;
+//     }
+//     message Chunk {
+//         bytes   data = 1;
+//         int32   offset = 2;
+//     }
+//     message FinalStatus {
+//         string  err = 1; // empty : success
+//     }
+//     oneof value {
+//         Meta            meta = 1;
+//         Chunk           chunk = 2;
+//         FinalStatus     final_status = 3;
+//     }
+// }
 // DownloadFile downloads a single file from servers out dir.
 func (s *TaurosServer) DownloadFile(req *api.DownloadFileReq, stream api.Tauros_DownloadFileServer) (err error) {
 	log.Printf("DownloadFile")
--- internal/server/file_upload.go
+++ internal/server/file_upload.go
@@ -6,6 +6,16 @@ import (
 	"log"
 )
 
+// message NegotiateUploadFilesReq {
+//     message File {
+//         string  filename = 1; // relative to bin dir
+//         bytes   sha256 = 2;
+//     }
+//     repeated File   files = 1; // files the client has
+// }
+// message NegotiateUploadFilesResp {
+//     repeated int32  indices = 1; // files the server doesn't have yet
+// }
 // NegotiateUploadFiles shall be called before every RunCommand() to possibly upload (further) files via one or more UploadFile().
 func (s *TaurosServer) NegotiateUploadFiles(ctx context.Context, req *api.NegotiateUploadFilesReq) (resp *api.NegotiateUploadFilesResp, err error) {
 	log.Printf("NegotiateUploadFiles")
@@ -13,11 +23,29 @@ func (s *TaurosServer) NegotiateUploadFiles(ctx context.Context, req *api.Negoti
 	return &api.NegotiateUploadFilesResp{}, nil
 }
 
+// message UploadFileReqStream {
+//     message Meta {
+//         string  filename = 1; // relative to bin dir
+//         int32   filesize = 2;
+//     }
+//     message Chunk {
+//         bytes   data = 1;
+//         int32   offset = 2;
+//     }
+//     oneof value {
+//         Meta    meta = 1;
+//         Chunk   chunk = 2;
+//     }
+// }
+
+// message UploadFileResp {
+//     string  err = 1; // empty : success
+// }
 // UploadFile uploads a single file to servers bin dir.
 func (s *TaurosServer) UploadFile(stream api.Tauros_UploadFileServer) (err error) {
 	log.Printf("UploadFile")
 
 	return stream.SendAndClose(&api.UploadFileResp{
-		ErrorMessage: "Failed",
+		Err: "Failed",
 	})
 }
